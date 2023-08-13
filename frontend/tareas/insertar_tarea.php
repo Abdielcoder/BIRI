0847cd2af367a0a8b36c3881007b1dca5914efb4
Abdiel Carrasco <dev@abdiel.me>
2023-08-12 21:47:30 -0700

    SWEET ALERT ADDED ON insertar_tarea

--- frontend/tareas/insertar_tarea.php
+++ frontend/tareas/insertar_tarea.php
@@ -1,46 +1,48 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Tu página</title>
+    <!-- Agrega el enlace al archivo JavaScript de SweetAlert -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/sweetalert/2.1.2/sweetalert.min.js"></script>
+</head>
+<body>
 <?php
-// Datos de conexión a la base de datos
+// Resto de tu código PHP aquí
 $servername = "localhost";
 $username = "root";
 $password = "";
-$dbname = "incidencias"; // Cambia esto al nombre de tu base de datos
+$dbname = "incidencias";
 
-// Crear una conexión a la base de datos
 $conn = new mysqli($servername, $username, $password, $dbname);
 
-// Verificar la conexión
 if ($conn->connect_error) {
     die("Conexión fallida: " . $conn->connect_error);
 }
 
-// Obtener los datos del formulario
 $reporta = $_POST["reporta"];
 $atiende = $_POST["atiende"];
 $departamentos = $_POST["departamentos"];
 $observaciones = $_POST["observaciones"];
 $fechaActual = date("Y-m-d");
-$timestamp = time(); // Obtiene el timestamp actual
-
-// Aquí puedes usar $timestamp para insertarlo en una base de datos u otra operación
-echo "Timestamp actual: " . $timestamp;
-
-// Si deseas formatear el timestamp como una fecha legible, puedes usar la función date()
+$timestamp = time();
 $fecha_legible = date("Y-m-d H:i:s", $timestamp);
-echo "Fecha legible: " . $fecha_legible;
-// Consulta SQL para insertar los datos en la tabla "tarea"
+
 $sql = "INSERT INTO tarea (nomcl, apecl, nomcas, sitio, state, dia, celu)
         VALUES ('$reporta', '$atiende', '$observaciones', '$departamentos', '0', '$fecha_legible', '6631231673')";
 
 if ($conn->query($sql) === TRUE) {
-    echo '<script>alert("Incidencia Creada!!!!")</script>'; 
-    echo "<script>
-            setTimeout(function() {
-                window.location.href = 'mostrar.php';
-            }, 3000);
-          </script>";
+    echo '<script type="text/javascript">
+        swal("Éxito", "Registro agregado correctamente", "success").then(function() {
+            window.location = "mostrar.php";
+        });
+    </script>';
 } else {
-    echo "Error al insertar los datos: " . $conn->error;
+    echo '<script type="text/javascript">
+        swal("Error", "Error al insertar los datos: ' . $conn->error . '", "error");
+    </script>';
 }
-// Cerrar la conexión
+
 $conn->close();
 ?>
+</body>
+</html>
